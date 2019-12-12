13cc12082b19ec81f7c950fd600e95d2c60e6bdd
sage-jordan <sagemjrodan@gmail.com>
2019-12-11 21:49:39 -0500

    MVP token used to unlock GET users and users/:id

--- api/apiRouter.js
+++ api/apiRouter.js
@@ -1,8 +1,10 @@
 const router = require('express').Router();
 
 const authRouter = require('../auth/authRouter');
+const userRouter = require('../users/userRouter');
 
 router.use('/auth', authRouter);
+router.use('/users', userRouter);
 
 router.get('/', (req, res) => {
     console.log(`GET to apiRouter`);
--- auth/auth-middleware.js
+++ auth/auth-middleware.js
@@ -1,7 +1,18 @@
+const jwt = require('jsonwebtoken');
+const secrets = require('../config/secrets');
+
 module.exports = (req, res, next) => {
-    if(req.session && req.session.user){
-        next();
+    const token = req.headers.authorization;
+    if(token) {
+        jwt.verify(token, secrets.jwtSecret, (err, decodedToken) => {
+            if(err) {
+                res.status(401).json({ you: "shall not pass!"});
+            } else {
+                req.decodedJwt = decodedToken;
+                next();
+            }
+        })
     } else {
-        res.status(401).json({ message: 'You shall not pass!'});
+        res.status(401).json({ you: "can't touch that."});
     }
 };
